86c1416c84731cd7aed86d23e7c440050d935900
David Cantoran <davidcantoran36@gmail.com>
2019-11-23 02:23:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,19 +2,19 @@
 
 def countdown(n)
   while n > 0
-  puts "#{n} SECOND(S)!"
-  n -= 1
-end
-return "HAPPY NEW YEAR!"
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
   while n > 0
-  sleep 1
-  puts "#{n} SECOND(S)!"
+    sleep 1
+    puts "#{n} SECOND(S)!"
   
-  n -= 1
+    n -= 1
   
-end
-return "HAPPY NEW YEAR!"
+  end
+  return "HAPPY NEW YEAR!"
 end
